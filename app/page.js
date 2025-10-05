2f4f13f4c7cfc9dd4370196a6ce620606fd268a2
Mahir Kumar <mahir.kumar@s.amity.edu>
2025-10-05 15:52:49 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <div className="flex justify-center flex-col gap-4 items-center text-white h-[44vh] px-5 md:px-0 bg-cover bg-center text-xs md:text-base lg:text-lg">
-        <div className="font-bold flex gap:6 md:gap-20 md:text-5xl justify-center items-center text-3xl ">Buy Me a Chai <span><img className="invertImg" src="/tea.gif" width={88} alt="" /></span></div>
+        <div className="font-bold flex gap:6 md:gap-20 md:text-5xl justify-center items-center text-3xl ">Buy Me a Chai <span><Image className="invertImg" src="/tea.gif" width={88} alt="" /></span></div>
         <p className="text-center md:text-left max-w-2xl">
           A crowdfunding platform for creators to receive support from their fans.
         </p>
@@ -30,15 +30,15 @@ export default function Home() {
         <h1 className="text-2xl font-bold text-center mb-14 text-center">Your Fans can buy you a Chai</h1>
         <div className="flex gap-5 justify-around">
           <div className="item space-y-3 flex flex-col justify-center items-center">
-            <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/man.png" alt="" />
+            <Image className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/man.png" alt="" />
             <p className="font-bold text-center">Fund yourself</p>
           </div>
           <div className="item space-y-3 flex flex-col justify-center items-center">
-            <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/coin.gif" alt="" />
+            <Image className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/coin.gif" alt="" />
             <p className="font-bold text-center">Fund yourself</p>
           </div>
           <div className="item space-y-3 flex flex-col justify-center items-center">
-            <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/group.gif" alt="" />
+            <Image className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/group.gif" alt="" />
             <p className="font-bold text-center">Fans want to help</p>
             <p className="text-center">Your fans are available for you to help you</p>
           </div>
